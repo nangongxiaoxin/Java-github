8afd530b5dc937bc81f09b9ca502c673e0a389eb
nangongxiaoxin <1764759472@qq.com>
2021-02-13 17:47:20 +0800

    0213

--- 截取字符串-P84/src/Subs.java
+++ 截取字符串-P84/src/Subs.java
@@ -1,4 +1,8 @@
 
-public class Subs {
-
+public class Subs {	//创建类
+	public static void main(String[] args) { //主方法
+		String str = "hello world";//定义字符串
+		String substr = str.substring(0,3);//对字符串进行剪切
+		System.out.println(substr);//输出剪切后的字符串 输出0,1,2对应的字符
+	}
 }
